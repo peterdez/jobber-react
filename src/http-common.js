43f913fe279b0ba6305680d82e2ca738d48617a2
peterdez <adebanjopete@gmail.com>
2023-11-23 23:45:18 +0100

    Update axios url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://jobber-api.vercel.app/api",
+  baseURL: "https://pacific-caverns-43958-c025eed56607.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
