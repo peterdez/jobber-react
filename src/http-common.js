eafd9097035ae3e4fa450a6ee62b0765f8e55ae5
peterdez <adebanjopete@gmail.com>
2023-11-23 14:20:48 +0100

    Update axios url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://jobber-react-4d3gz4rcu-peterdez.vercel.app/api",
   headers: {
     "Content-type": "application/json"
   }
